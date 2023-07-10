d0ff6b6c742e1e9e77f6af3fb32ed659de514fb7
blau <serzh.plichko@mail.ru>
2023-07-10 18:02:35 +1100

    hw 1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,26 @@
--- SQL-команды для создания таблиц
+CREATE TABLE customer_data
+(
+    customer_id varchar(5) PRIMARY KEY,
+	company_name text,
+	contact_name text
+);
+
+CREATE TABLE employees_data
+(
+    employee_id int PRIMARY KEY,
+	first_name varchar(100),
+	last_name varchar(100),
+	title varchar(100),
+	birth_date date,
+	notes text
+);
+
+
+CREATE TABLE order_data
+(
+    order_id int PRIMARY KEY,
+	customer_id varchar(5) REFERENCES customer_data(customer_id),
+	employee_id int REFERENCES employees_data(employee_id),
+	order_date date,
+	ship_city varchar(100)
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,42 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+import psycopg2
+
+file_path_customer = 'north_data/customers_data.csv'
+file_path_order = 'north_data/orders_data.csv'
+file_path_employees = 'north_data/employees_data.csv'
+
+conn = psycopg2.connect(host='localhost', database='north', user='postgres', password='Ps200028')
+try:
+    with conn:
+        with conn.cursor() as cur:
+            with open(file_path_customer, 'r', encoding='utf-8') as file:
+                reader = csv.reader(file)
+                next(reader)
+                cur.executemany(
+                    'INSERT INTO customer_data (customer_id, company_name, contact_name)'
+                    'VALUES (%s, %s, %s)',
+                    reader
+                )
+            with open(file_path_order, 'r', encoding='utf-8') as file:
+                reader = csv.reader(file)
+                next(reader)
+                cur.executemany(
+                    'INSERT INTO order_data (order_id, customer_id, employee_id, order_date, ship_city)'
+                    'VALUES (%s, %s, %s, %s, %s)',
+                    reader
+                )
+            with conn.cursor() as cur:
+                with open(file_path_employees, 'r', encoding='utf-8') as file:
+                    reader = csv.reader(file)
+                    next(reader)
+                    cur.executemany(
+                        'INSERT INTO employees_data (employee_id, first_name, last_name, title, birth_date, notes)'
+                        'VALUES (%s, %s, %s, %s, %s, %s)',
+                        reader
+                    )
+
+
+finally:
+    conn.close()
+
