a6da1fe6a19d9396388e3294bc0babcadae9734d
blau <serzh.plichko@mail.ru>
2023-07-15 15:24:13 +1100

    hw 2 fixed

--- homework-2/filter_sorting.sql
+++ homework-2/filter_sorting.sql
@@ -11,9 +11,10 @@ ORDER BY ship_country, ship_city;
 
 -- 3. сколько дней в среднем уходит на доставку товара в Германию (таблица orders, колонки order_date, shipped_date, ship_country)
 
-SELECT shipped_date - order_date as time_wait, ship_country
+SELECT AVG(shipped_date - order_date) as avg_time_wait, ship_country
 FROM orders
-
+WHERE ship_country = 'Germany'
+GROUP BY ship_country
 
 -- 4. минимальную и максимальную цену среди продуктов, не снятых с продажи (таблица products, колонки unit_price, discontinued не равно 1)
 
